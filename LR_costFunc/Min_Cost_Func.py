4ff65f58f61435da9c8656b72affe76e7bf0499c
lkys221 <lkys221@snu.ac.kr>
2018-08-03 17:56:58 +0900

    min cost func

--- LR_costFunc/Min_Cost_Func.py
+++ LR_costFunc/Min_Cost_Func.py
@@ -7,4 +7,17 @@ Y = [1, 2, 3]
 W = tf.placeholder(tf.float32)
 hypothesis = W * X
 
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+sess = tf.Session()
+
+W_val = []
+cost_val = []
+
+for i in range(-30, 50):
+    feed_W = i * 0.1
+    curr_cost, curr_W = sess.run([cost, W], feed_dict={W: feed_W})
+    W_val.append(curr_W)
+    cost_val.append(curr_cost)
+
+
 
