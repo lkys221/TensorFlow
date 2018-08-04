dfc5e30623e1dc5780d21532aa8fea12be4fd519
lkys221 <lkys221@snu.ac.kr>
2018-08-04 19:09:53 +0900

    min cost function

--- LR_costFunc/Min_Cost_Func.py
+++ LR_costFunc/Min_Cost_Func.py
@@ -8,7 +8,16 @@ X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
 
 hypo = W * X
-cost = tf.reduce_mean(tf.square(hypo = Y))
+cost = tf.reduce_mean(tf.square(hypo - Y))
 
 learning_rate = 0.1
 gradient = tf.reduce_mean((W * X - Y) * X)
+descent = W - learning_rate * gradient
+update = W.assign(descent)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for step in range(21):
+    sess.run(update, feed_dict={X: x_data, Y: y_data})
+    print(step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(W))
