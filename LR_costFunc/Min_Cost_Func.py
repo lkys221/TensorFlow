a9737b266f3bfec220f6aaf0779b37f5f027ee56
lkys221 <lkys221@snu.ac.kr>
2018-08-04 19:27:54 +0900

    LR multi val

--- LR_costFunc/Min_Cost_Func.py
+++ LR_costFunc/Min_Cost_Func.py
@@ -10,10 +10,16 @@ Y = tf.placeholder(tf.float32)
 hypo = W * X
 cost = tf.reduce_mean(tf.square(hypo - Y))
 
+# minimize
 learning_rate = 0.1
 gradient = tf.reduce_mean((W * X - Y) * X)
 descent = W - learning_rate * gradient
 update = W.assign(descent)
+
+# minimize (equivalent)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+train = optimizer.minimize(cost)
+
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
