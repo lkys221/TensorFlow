b2db1f5576295623fbf96554e581303b083cf9cc
lkys221 <lkys221@snu.ac.kr>
2018-08-06 17:43:29 +0900

    LR csv

---
+++ LR_costFunc/LR_csv.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+tf.set_random_seed(777)  # for reproducibility
+
+xy = np.loadtxt('./data/data-01-test-score.csv', delimiter=',', dtype=np.float32)
+x_data = xy[:, 0:-1]
+y_data = xy[:, [-1]]
+
+X = tf.placeholder(tf.float32, shape=[None, 3])
+Y = tf.placeholder(tf.float32, shape=[None, 1])
+
+W = tf.Variable(tf.random_normal([3, 1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+hypothesis = tf.matmul(X, W) + b
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
+train = optimizer.minimize(cost)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for step in range(2001):
+    cost_val, hy_val, _ = sess.run(
+        [cost, hypothesis, train], feed_dict={X: x_data, Y: y_data})
+    if step % 40 == 0:
+        print("Step: ", step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
+
+print("Your score will be ", sess.run(
+    hypothesis, feed_dict={X: [[100, 70, 101]]}))
