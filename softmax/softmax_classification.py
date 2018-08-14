897acfa2ec1a06958b41241efb1b6b8c67136cd4
lkys221 <lkys221@snu.ac.kr>
2018-08-15 00:03:43 +0900

    softmax

---
+++ softmax/softmax_classification.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+x_data = [[1, 2, 1, 1],
+          [2, 1, 3, 2],
+          [3, 1, 3, 4],
+          [4, 1, 5, 5],
+          [1, 7, 5, 5],
+          [1, 2, 5, 6],
+          [1, 6, 6, 6],
+          [1, 7, 7, 7]]
+
+y_data = [[0, 0, 1],
+          [0, 0, 1],
+          [0, 0, 1],
+          [0, 1, 0],
+          [0, 1, 0],
+          [0, 1, 0],
+          [1, 0, 0],
+          [1, 0, 0]]
+
+X = tf.placeholder("float", [None, 4])
+Y = tf.placeholder("float", [None, 3])
+
+W = tf.Variable(tf.random_normal([4, 3]), name='weight')
+b = tf.Variable(tf.random_normal([3]), name='bias')
+
