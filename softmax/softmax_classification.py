fe3579e59615d5e3591dca98a22ee1924e9d13e8
lkys221 <lkys221@snu.ac.kr>
2018-08-15 11:42:11 +0900

    softmax

--- softmax/softmax_classification.py
+++ softmax/softmax_classification.py
@@ -19,8 +19,26 @@ y_data = [[0, 0, 1],
           [1, 0, 0]]
 
 X = tf.placeholder("float", [None, 4])
-Y = tf.placeholder("float", [None, 3])
+Y = tf.placeholder("float", [None, 3])  
 
 W = tf.Variable(tf.random_normal([4, 3]), name='weight')
 b = tf.Variable(tf.random_normal([3]), name='bias')
+
+hypothesis = tf.nn.softmax(tf.matmul(X, W) + b)
+cost = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(hypothesis), axis=1))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(2001):
+        sess.run(optimizer, feed_dict={X: x_data, Y: y_data})
+        if step % 400 == 0:
+            print(step, sess.run(cost, feed_dict={X: x_data, Y: y_data}))
+
+    print('--------------')
+
+    # Testing & One-hot encoding
+    a = sess.run(hypothesis, feed_dict={X: [[1, 11, 7, 9]]})
+    print(a, sess.run(tf.argmax(a, 1)))
 
