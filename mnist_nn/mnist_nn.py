0939a9854fe303411292c07da3c710e67ffdd3f7
lkys221 <lkys221@snu.ac.kr>
2018-09-01 16:53:18 +0900

    mnist_nn complete

--- mnist_nn/mnist_nn.py
+++ mnist_nn/mnist_nn.py
@@ -41,3 +41,47 @@ W5 = tf.get_variable("W5", shape=[512, 10], initializer=tf.contrib.layers.xavier
 b5 = tf.Variable(tf.random_normal([10]))
 
 hypothesis = tf.matmul(L4, W5) + b5
+
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=hypothesis, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        feed_dict = {X: batch_xs, Y: batch_ys, keep_prob: 0.7}
+        c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
+        avg_cost += c / total_batch
+
+    print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
+
+print('Learning Finished!')
+
+
+correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print('Accuracy:', sess.run(accuracy, feed_dict={
+      X: mnist.test.images, Y: mnist.test.labels, keep_prob: 1}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
